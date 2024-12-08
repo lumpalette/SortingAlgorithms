64c477d6142e219bf18d5db7d8df82c3ac1c9b6f
lumPalette <lumPalette@gmail.com>
2024-12-07 19:44:39 -0600

    added comments to the algorithms

--- src/Algorithms.cs
+++ src/Algorithms.cs
@@ -8,9 +8,10 @@ public static class Algorithms
 	/// <param name="array">El array a ordenar.</param>
 	public static void BubbleSort(int[] array)
 	{
-		// frick you
 		for (int i = 0; i < array.Length - 1; i++)
 		{
+			// Compara elementos adyacentes para comprobar si el elemento actual (j) es mayor a su 
+			// sucesor; si ese es el caso, los intercambia. El algoritmo ordena la lista de final a inicio.
 			bool swapped = false;
 			for (int j = 0; j < array.Length - i - 1; j++)
 			{
@@ -20,6 +21,8 @@ public static class Algorithms
 					swapped = true;
 				}
 			}
+			
+			// Si no se intercambio ningún elemento, significa que la lista está ordenada.
 			if (!swapped)
 			{
 				break;
@@ -33,16 +36,23 @@ public static class Algorithms
 	/// <param name="array">El array a ordenar.</param>
 	public static void SelectionSort(int[] array)
 	{
+		// Este algoritmo busca el elemento más pequeño dentro de la sublista no ordenada
+		// y lo posiciona en su lugar correspondiente.
 		for (int i = 0; i < array.Length - 1; i++)
 		{
+			// Se asume que el elemento 'i' es el menor.
 			int minIndex = i;
 			for (int j = i + 1; j < array.Length; j++)
 			{
+				// Actualizar minIndex si existe un elemento
 				if (array[j] < array[minIndex])
 				{
 					minIndex = j;
 				}
 			}
+
+			// El elemento más pequeño de la sublista se posiciona correctamente y se remueve de la sublista
+			// no ordenada.
 			(array[minIndex], array[i]) = (array[i], array[minIndex]);
 		}
 	}
@@ -57,11 +67,16 @@ public static class Algorithms
 		{
 			int current = array[i];
 			int j = i - 1;
-			while (j >= 0 && array[j] > current)
+
+			// Mientras haya elementos en la parte ordenada y el valor actual sea menor que el elemento comparado...
+			while ((j >= 0) && (array[j] > current))
 			{
+				// ... desplazamos el elemento hacia la derecha para hacer espacio para el elemento actual.
 				array[j + 1] = array[j];
 				j--;
 			}
+
+			// Insertamos el elemento actual en su posición correcta dentro de la parte ordenada.
 			array[j + 1] = current;
 		}
 	}
@@ -72,9 +87,10 @@ public static class Algorithms
 	/// <param name="array">El array a ordenar.</param>
 	public static void QuickSort(int[] array, int low, int high)
 	{
-		// dude wtf how is this thing so fast
 		int i = low;
 		int j = high;
+
+		// El pivote se elige como el primer elemento del array.
 		int pivot = array[low];
 		while (i <= j)
 		{
@@ -86,17 +102,25 @@ public static class Algorithms
 			{
 				j--;
 			}
+
+			// Si 'i' aún no ha sobrepasado a 'j', intercambiamos los elementos en 'i' y 'j'
 			if (i <= j)
 			{
 				(array[j], array[i]) = (array[i], array[j]);
+
+				// Se desplazan los índices para procesar los elementos siguientes y anteriores.
 				i++;
 				j--;
 			}
 		}
+
+		// Se ordena la partición izquierda si es necesario.
 		if (low < j)
 		{
 			QuickSort(array, low, j);
 		}
+
+		// Se ordena la partición derecha si es necesario.
 		if (i < high)
 		{
 			QuickSort(array, i, high);
